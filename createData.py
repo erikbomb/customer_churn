fc0ab522a0efd5ad189bc64d028e171b29ad3158
Erik <eschweiss@gmail.com>
2022-03-20 20:03:55 -1000

    Initial Setup of the project.  Very slow with manual looks at each customer for every month by one thread

---
+++ createData.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+def random_sheet(name):
+    #make a quick random date selector
+    dates= []
+    for i in range(0,365*5):
+        dates.append(np.datetime64('2016-01-01') + i)
+    
+    # setting the number of rows for the CSV file
+    N = 1000000
+    # make the table outline with random numbers
+    df = pd.DataFrame(np.random.randint(999,999999,size=(N, 4)), columns=["CustomerNumber","PurchaseDate","TotalItems","InvoiceTotal"])
+    # randomize the purchase date, totalItems, Invoice total
+    df["PurchaseDate"] = np.random.choice(dates, N)
+    df["TotalItems"] = np.random.randint(1,100, N)
+    df["InvoiceTotal"] = np.random.uniform(1.00,10000.00,N)
+    df["InvoiceTotal"] = np.round(df["InvoiceTotal"],decimals= 2)
+    # print the dataframe to see what we have created
+    print(df)
+    # export the dataframe to csv using comma delimiting
+    df.to_csv('random'+name+'.csv', sep=",")
+
+num_sheets=5
+for i in range(0,num_sheets):
+    random_sheet(str(i))
---
+++ customerChrun.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy as np
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+from datetime import datetime
+from tqdm import tqdm
+import time
+
+#name of columns that I need
+purchase_date='PurchaseDate'
+customer_number="CustomerNumber"
+total_items="TotalItems"
+invoice_total="InvoiceTotal"
+
+        
+# Borrowed to display time it takes to run the program
+intervals = (
+    ('weeks', 604800),  # 60 * 60 * 24 * 7
+    ('days', 86400),    # 60 * 60 * 24
+    ('hours', 3600),    # 60 * 60
+    ('minutes', 60),
+    ('seconds', 1),
+)
+
+def display_time(seconds, granularity=2):
+    result = []
+
+    for name, count in intervals:
+        value = seconds // count
+        if value:
+            seconds -= value * count
+            if value == 1:
+                name = name.rstrip('s')
+            result.append("{} {}".format(value, name))
+    return ', '.join(result[:granularity])
+
+def print_message(message, message_type=0, location=0):
+    # 0 - regular, 1 - warning, 2 - missing
+    types= ["[ ] - ","[!] - ","      "]
+    # location 0 - console , 1 - logs, 2 - both
+    print(types[message_type] + message)
+
+def get_files():
+    Tk().withdraw()
+    return askopenfilename()
+
+def export_to_csv(excel):
+    excel.to_csv('customer_churn.csv', sep=",")
+
+def calculate(df):
+    #turbo
+    #fix the data
+    print_message("Convertings dates to date_time",0,0)
+    df[purchase_date]  = pd.to_datetime(df[purchase_date], format='%Y-%m-%d')
+    print_message("dates converted",0,0)
+    #get the number of months to iterate through
+    start_date=pd.to_datetime(min(df[purchase_date]))
+    end_date=pd.to_datetime(max(df[purchase_date]))
+    # had to add 1 to make sure that it includes both months
+    number_of_months=np.int(np.floor(((end_date - start_date)/np.timedelta64(1,'M'))))+1
+    unique_customers=df[customer_number].unique().tolist()
+    total_customer_count = len(unique_customers)
+    column_names = ["month","returning_customers","lost_customers","gained_customers","total_customers"]
+    churn_data = pd.DataFrame(columns = column_names)
+    #go through monthly and figure out the stuff...
+    for month in range(0,number_of_months + 1):
+        print_message("Starting month number " +str(month),0,0)
+        # set the start and end of a month
+        previous_month_start =pd.to_datetime(start_date + pd.DateOffset(months=month-1))
+        current_month_start =pd.to_datetime(start_date + pd.DateOffset(months=month))
+        current_month_end =pd.to_datetime(start_date + pd.DateOffset(months=month+1))
+        #add the new month to the df
+        new_row = {'month':current_month_start, 'returning_customers':0,'gained_customers':0,'lost_customers':0,'total_customers':0}
+        for customer in tqdm(unique_customers):
+            check_customer(customer,new_row,df,previous_month_start,current_month_start,current_month_end)
+        churn_data = churn_data.append(new_row, ignore_index=True)
+        print (churn_data.iloc[-1])
+    pool.close()
+    pool.join()
+    return churn_data
+def check_customer(customer,new_row,df,previous_month_start,current_month_start,current_month_end):
+    #get current customer 
+    customer_data = df.loc[df[customer_number] == customer]
+    #check for any purchases and record for this month
+    purchase_this = customer_data.loc[(customer_data[purchase_date] >= current_month_start) & (customer_data[purchase_date] < current_month_end)]
+    purchase_this_month = not purchase_this.empty
+    #check for any purchases and record before this month
+    purchase_prior = customer_data.loc[(customer_data[purchase_date] < current_month_start)]
+    purchase_prior_month = not purchase_prior.empty
+    #check for any purchases and record after this month
+    purchase_future = customer_data.loc[(customer_data[purchase_date] >= current_month_end)]
+    purchase_future_month = not purchase_future.empty
+    #check if we lost the customer this month - ie last month was the last purchase record
+    if not purchase_future_month:
+        last_month_customer = not customer_data.loc[(customer_data[purchase_date] >= previous_month_start) & (customer_data[purchase_date] < current_month_start)].empty
+    else:
+        last_month_customer = False
+    #set the values in the dataframe
+    if purchase_prior_month and purchase_this_month:
+        new_row["returning_customers"] = new_row["returning_customers"] + 1
+    if not purchase_prior_month and purchase_this_month:
+        new_row["gained_customers"] = new_row["gained_customers"] + 1
+    if last_month_customer and not purchase_this_month and not purchase_future_month:
+        new_row["lost_customers"] = new_row["lost_customers"] + 1
+    if purchase_prior_month or purchase_this_month:
+        new_row["total_customers"] = new_row["total_customers"] + 1
+
+def setup():
+    # allow user to keep getting files
+    filename = "blank"
+    files_list = []
+    files_list.append('data/random1.csv') 
+    # keep going and add files to array if it is not a blank file
+    # add this back in
+    #while filename:
+    #    filename=get_files()
+    #    if filename:
+    #        files_list.append(filename)
+    #if not files_list:
+    #   print_message("No files selected.. exiting",1,0)
+    #load in the data 
+    print_message("loading the following files:",0,1) 
+    frames = [] 
+    for i in files_list: 
+        print_message(i,2,1) 
+        frames.append(pd.read_csv(i)) 
+    return pd.concat(frames)
+
+def run():
+    df = setup()
+    excel=calculate(df)
+    export_to_csv(excel)
+
+# get the start time and then run, finish with total time
+start = time.time()
+run()
+display_time(time.time() -start_time))
+
